e9880da02fdbaa141a12ffeb0c20083c6338376b
e.levin <e.levin@solarlab.ru>
2025-05-26 01:25:03 +0300

    init

---
+++ src/main/java/com/pewde/familytree/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.pewde.familytree.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/pewde/familytree/controller/SimController.java
@@ -0,0 +1,9 @@
+package com.pewde.familytree.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/sim")
+public class SimController {
+}
